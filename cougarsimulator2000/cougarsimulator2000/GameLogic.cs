c6053cf44438875e9b209bbc3c3fd6ad818f80b3
MasterTaffer <odestrzo@gmail.com>
2016-03-17 15:16:04 +0200

    Implement Gunpowder Tex

--- cougarsimulator2000/cougarsimulator2000/GameLogic.cs
+++ cougarsimulator2000/cougarsimulator2000/GameLogic.cs
@@ -32,22 +32,47 @@ namespace cougarsimulator2000
             get;
         }
 
+        private List<Actor> cougars = new List<Actor>();
+
         public GameLogic()
         {
             
             actors = new List<Actor>();
             tileMap = new TileMap();
             tileMap.initialize(new Vector2(16, 16));
-
-            tileMap.getTile(new Vector2(2, 2)).type = 1;
+            for (int i = 1; i < 15; i++)
+                for (int j = 1; j < 15; j++)
+                    tileMap.getTile(new Vector2(i,j)).type = 1;
             player = new Actor();
-            player.image = "ac_cougar";
-            player.depth = 1;
+            player.image = "ac_tex";
+            player.depth = 2;
             player.position.x = 4;
             player.position.y = 4;
 
             actors.Add(player);
+
+            Random r = new Random();
+            for (int i = 0; i < 16; i++)
+            {
+                Actor a = new Actor();
+                a.image = "ac_cougar";
+                a.depth = 1;
+                a.position.x = r.Next(16);
+                a.position.y = r.Next(16);
+                cougars.Add(a);
+                actors.Add(a);
+            }
         }       
+
+        private void updateActors()
+        {
+            Random r = new Random();
+            foreach (var cugar in cougars)
+            {
+                cugar.position.x += r.Next(3) - 1;
+                cugar.position.y += r.Next(3) - 1;
+            }
+        }
 
         public void enterInput(Input input)
         {
@@ -66,6 +91,7 @@ namespace cougarsimulator2000
                 off.y++;
 
             player.position += off;
+            updateActors();
         }
     }
 }
