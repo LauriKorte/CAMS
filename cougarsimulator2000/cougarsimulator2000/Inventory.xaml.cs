c1655baf9cba39d256784017a85e9f371bc91415
Lauri Korte <lauri.korte@hotmail.com>
2016-04-02 18:43:37 +0300

    Stoory

--- cougarsimulator2000/cougarsimulator2000/Inventory.xaml.cs
+++ cougarsimulator2000/cougarsimulator2000/Inventory.xaml.cs
@@ -19,7 +19,6 @@ namespace cougarsimulator2000
     /// </summary>
     public partial class Inventory : Window
     {
-        Item it;
         public Inventory()
         {
             InitializeComponent();
